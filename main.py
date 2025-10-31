aecf2c5040d376143ad3c3f175a527f361fcffbc
GameBoi <thegameboi@proton.me>
2025-10-30 20:10:41 -0500

    Implemented some basic CSS to the html page for better visuals. Added a POST method to the main file.

--- main.py
+++ main.py
@@ -4,9 +4,23 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        print(request.form)
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+
     return render_template("index.html")
+
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 
 
 app.run(debug=True, port=5001)
